fadd83023f638f03de127718c1abe33869d51c18
Manoj Das <manojpdas@hotmail.com>
2021-04-29 22:14:03 -0400

    Initial Commit

---
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using  SimpleAPI.Controllers;
+
+namespace SimpleAPI.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
